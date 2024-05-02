25424299f404559b02f636cf4a476f7ffa87576f
W-Jeng <woonjeng@gmail.com>
2024-05-02 21:15:31 +0800

    updated pi estimator

--- multithreaded_pi_estimator/pi_estimator.cpp
+++ multithreaded_pi_estimator/pi_estimator.cpp
@@ -1,7 +1,15 @@
 #include "pi_estimator.h"
 #include<cmath>
 
-PiEstimator::PiEstimator(const double& temp_radius) :radius(temp_radius), square_area(temp_radius*temp_radius) {};
+PiEstimator::PiEstimator(const double& temp_radius) :radius(temp_radius), square_area(temp_radius*temp_radius), num_of_estimator(0), is_in_circle_count(0) {};
+
+void PiEstimator::refine_estimation(const double& x, const double& y) {
+    num_of_estimator++;
+    if (PiEstimator::is_in_circle(x, y)) {
+        is_in_circle_count++;
+    }
+    return;
+};
 
 bool PiEstimator::is_in_circle(const double& x, const double& y) {
     if (pow(pow(x, 2) + pow(y, 2), 0.5) < radius)
@@ -9,4 +17,9 @@ bool PiEstimator::is_in_circle(const double& x, const double& y) {
     else
         return false;
 };
+
+const double& PiEstimator::get_pi_estimation() {
+    return static_cast<double>(is_in_circle_count)/num_of_estimator;
+};
+
 
--- multithreaded_pi_estimator/pi_estimator.h
+++ multithreaded_pi_estimator/pi_estimator.h
@@ -4,9 +4,12 @@ class PiEstimator {
 public:
 				PiEstimator(const double& temp_radius);
 				bool is_in_circle(const double& x, const double& y);
+				void refine_estimation(const double& x, const double& y);
+				const double& get_pi_estimation();
 
 private:
 				const double radius;
 				const double square_area;
-
+				int num_of_estimator;
+				int is_in_circle_count;
 };
